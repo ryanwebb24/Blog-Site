e21cd007d34464fb763add834af75e3055ba8d78
ryanwebb24 <ryanwebb2444@gmail.com>
2023-04-03 17:35:22 -0600

    Style additions to website

--- website/auth.py
+++ website/auth.py
@@ -1,18 +1,24 @@
 """Creates the blueprints for the authentication relates to logging in then runs the html file associated,
     currently implemented with login, logout, and sign up"""
 
-from flask import Blueprint, redirect, render_template, url_for
+from flask import Blueprint, redirect, render_template, url_for, request
 
 auth = Blueprint("auth", __name__)
 
 
-@auth.route("/login")
+@auth.route("/login", methods=['GET','POST'])
 def login():
+    email = request.form.get("email")
+    password1 = request.form.get("password1")
     return render_template("login.html")
 
 
-@auth.route("/sign-up")
+@auth.route("/sign-up", methods=['GET','POST'])
 def signup():
+    email = request.form.get("email")
+    username = request.form.get("username")
+    password1 = request.form.get("password1")
+    password2 = request.form.get("password2")
     return render_template("signup.html")
 
 
