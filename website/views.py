fdee438976d3d0d42461e36ed484332ef5548264
ryanwebb24 <ryanwebb2444@gmail.com>
2023-04-03 20:14:13 -0600

    account and database creation

--- website/views.py
+++ website/views.py
@@ -1,11 +1,13 @@
 """Creates the blueprint for each url path that the user takes and calls the html file. So far only implemented home"""
 
 from flask import Blueprint, render_template
+from flask_login import login_required, current_user
 
 views = Blueprint("views", __name__)
 
 
 @views.route("/")
 @views.route("/home")
+@login_required
 def home():
-    return render_template("home.html")
+    return render_template("home.html", name=current_user.username)
